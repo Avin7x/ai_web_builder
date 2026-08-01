24229f8188c1f249675bec16a40dfd86ce1691b9
devin567 <avinnash567@gmail.com>
2026-08-01 20:57:18 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { AuthLayout, GuestLayout } from './pages/Layout'
+import AuthPage from './pages/AuthPage'
+import Homepage from './pages/Homepage'
+import BuilderPage from './pages/Builderpage'
+import PreviewPage from './pages/PreviewPage'
 
-function App() {
+const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      {/* Login Routes */}
+      <Route  element={<GuestLayout />} >
+        <Route path='/login' element={<AuthPage mode="login"/> }/>
+        <Route path='/register' element={<AuthPage mode="register"/> }/>
+      </Route>
+
+
+      {/* Protected Routes */}
+      <Route  element={<AuthLayout />} >
+        <Route path='/' element={<Homepage /> }/>
+        <Route path='/builder/:id' element={<BuilderPage /> }/>
+        <Route path='/preview/:id' element={<PreviewPage /> }/>
+      </Route>
+
+    </Routes>
   )
 }
 
