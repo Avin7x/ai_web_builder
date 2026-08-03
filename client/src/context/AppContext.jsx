6f01e602acbc71838b03e0ea2bf8a2c5b1e2c8b8
devin567 <avinnash567@gmail.com>
2026-08-03 17:32:09 +0530

    created user authentication forms

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,46 +1,85 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import api from "../api/api";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext(undefined);
 
 export function AppContextProvider({ children }) {
-    // Auth states
-    const [user, setUser] = useState(null);
-    const [loadingUser, setLoadingUser] = useState(true);
+  // Auth states
+  const [user, setUser] = useState(null);
+  const [loadingUser, setLoadingUser] = useState(true);
 
-    // Auth actions
-    const checkSession = async () => {
-        try {
-            const { data } = await api.get("/api/auth/me");
-            // setUser(data.user);
-        } catch (error) {
-            setUser(null);
-        } finally {
-            setLoadingUser(false);
-        }
+  const navigate = useNavigate();
+
+  // Auth actions
+  const checkSession = async () => {
+    try {
+      const { data } = await api.get("/api/auth/me");
+      setUser(data.user);
+    } catch (error) {
+      setUser(null);
+    } finally {
+      setLoadingUser(false);
     }
+  };
 
-    useEffect(()=>{
-        checkSession();
-    }, [checkSession])
+  useEffect(() => {
+    checkSession();
+  }, [checkSession]);
 
-    return (
-        <AppContext.Provider
-            value={{
-                user,
-                setUser,
-                loadingUser,
-                setLoadingUser,
-            }}
-        >
-            {children}
-        </AppContext.Provider>
-    )
+  const login = async (email, password) => {
+    try {
+      const { data } = await api.post("/api/auth/login", { email, password });
+      setUser(data.user);
+      toast.success("Welcome back!");
+      navigate("/");
+    } catch (error) {
+      console.log("Login failed:", error);
+      const errMsg =
+        error.response?.data?.message || "Invalid email or password";
+      toast.error(errMsg);
+      throw new Error(errMsg);
+    }
+  };
+  const register = async (name, email, password) => {
+    try {
+      const { data } = await api.post("/api/auth/register", {
+        name,
+        email,
+        password,
+      });
+      setUser(data.user);
+      toast.success("Account created successfully!");
+      navigate("/");
+    } catch (error) {
+      console.log("Login failed:", error);
+      const errMsg = error.response?.data?.message || "Registration failed";
+      toast.error(errMsg);
+      throw new Error(errMsg);
+    }
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        setUser,
+        loadingUser,
+        setLoadingUser,
+        login,
+        register,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 export function useAppContext() {
-    const context = useContext(AppContext);
-    if (!context) throw new Error("useAppContext must be used within an AppContextProvider");
+  const context = useContext(AppContext);
+  if (!context)
+    throw new Error("useAppContext must be used within an AppContextProvider");
 
-    return context;
+  return context;
 }
